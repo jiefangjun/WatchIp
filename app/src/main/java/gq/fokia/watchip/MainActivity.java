6f8ebb92632e8f8b8d37bf7c738e1681fd78f5ba
fokia <fokia@qq.com>
2017-02-14 02:02:10 +0800

    add transparent statusbar

--- app/src/main/java/gq/fokia/watchip/MainActivity.java
+++ app/src/main/java/gq/fokia/watchip/MainActivity.java
@@ -3,10 +3,15 @@ package gq.fokia.watchip;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -30,12 +35,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public CheckBox vibration;
     public Boolean bvoice = false;
     public Boolean bvibration = false;
+    private Toolbar toolbar;
     private SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //appManager.addActivity(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//5.0 全透明实现
+//getWindow.setStatusBarColor(Color.TRANSPARENT)
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //4.4 全透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tb);
+        setSupportActionBar(toolbar);
         pref = getSharedPreferences("data", 0);
         textView = (TextView) findViewById(R.id.ip);
         textView.setOnClickListener(this);
